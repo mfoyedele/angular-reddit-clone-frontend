2b8446435898ede9ec7ca8da2ada0f660e9c4fe7
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-13 19:48:41 +0100

    Update app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { IgxGridModule, IgxIconModule } from 'igniteui-angular';
 import { AppRoutingModule } from './app-routing.module';
 
 import { PagingComponent, SortingComponent, FilteringComponent, EditingComponent } from './home';
 
-import { FormsModule } from "@angular/forms";
-
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
-import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
 
 @NgModule({
     imports: [
-        BrowserModule,
-        FormsModule,
-        IgxIconModule,
-        IgxPreventDocumentScrollModule,
-        BrowserAnimationsModule,
-        IgxGridModule,
+        BrowserModule,        
         AppRoutingModule
     ],
     declarations: [

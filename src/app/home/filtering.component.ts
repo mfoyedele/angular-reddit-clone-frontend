e0bd40bf8f7c29d4acf8122aed70868811b7a19a
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-13 19:38:16 +0100

    Update filtering component

--- src/app/home/filtering.component.ts
+++ src/app/home/filtering.component.ts
@@ -1,25 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { DATA } from '../data/nwindData';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-grid-filtering-style',
   styleUrls: ['./filtering.component.scss'],
   templateUrl: './filtering.component.html'
 })
-export class FilteringComponent implements OnInit {
-    public data: any[];
-
-    constructor() {
-    }
-    public ngOnInit(): void {
-        this.data = DATA;
-    }
-
-    public formatDate(val: Date) {
-        return new Intl.DateTimeFormat('en-US').format(val);
-    }
-
-    public formatCurrency(val: string) {
-        return parseInt(val, 10).toFixed(2);
-    }
-}
+export class FilteringComponent {} 

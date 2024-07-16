73b6810d059ff5448a6a762840e0349b9138ddd8
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-16 17:43:51 +0100

    Update component.ts

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+
+  signupForm: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+
+    })
+  }
 
 }

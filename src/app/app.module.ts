e2128b201300043f3a406e845daf152949a76660
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-16 17:36:43 +0100

    Adjust routing module settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
-        HeaderComponent
+        HeaderComponent,
+        SignupComponent
         
     ],
     bootstrap: [AppComponent]

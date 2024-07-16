c075c0cc44ab1b8c427020f342d4b19e156dc60a
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-16 18:20:10 +0100

    Update auth.service class

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SignupRequestPayload } from '../signup/singup-request.payload';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  signup(signupRequestPayload: SignupRequestPayload) : Observable<any>{
+    return this.httpClient.post('http://localhost:8080/api/auth/signup', signupRequestPayload, {responseType: 'text'});
+  }
 }

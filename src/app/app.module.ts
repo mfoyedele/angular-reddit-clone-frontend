32d80420968b576b5c658bb32f1240df1c196a09
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-16 02:58:40 +0100

    Update header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
     ],
     declarations: [
         AppComponent,
+        HeaderComponent
         
     ],
     bootstrap: [AppComponent]

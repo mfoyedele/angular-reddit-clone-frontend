989d60fac0cc0b3ae25d50f4fce595f5d2ffadd6
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-18 15:57:03 +0100

    Update app

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
-import { AppComponent } from '@app/app.component';
-import { APP_ROUTES } from '@app/app.routes';
+import { AppComponent } from './app/app.component';
+import { APP_ROUTES } from './app/app-routes.module';
 
 bootstrapApplication(AppComponent, {
   providers: [

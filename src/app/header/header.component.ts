e47da3d2329186c3fe064d241b7ac2678e4988e5
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-18 15:31:01 +0100

    Update header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIf, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  templateUrl: './header.component.html',  
+  styleUrl: './header.component.css',
+  standalone: true,
+  imports: [NgIf, NgClass]
 })
 export class HeaderComponent {
 

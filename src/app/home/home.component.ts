2d6b4355ac4f0b1cf413cde80a15b5c76df994a0
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-19 17:04:49 +0100

    Update home changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,14 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { LoginResponse } from '@app/_models';
 import { AccountService } from '@app/_services';
+import { HeaderComponent } from '@app/header/header.component';
 
 @Component({
     templateUrl: 'home.component.html',
-    standalone: true
+    standalone: true,    
+    imports: [NgClass, NgIf, HeaderComponent]
 })
 export class HomeComponent {
     user: LoginResponse | null;

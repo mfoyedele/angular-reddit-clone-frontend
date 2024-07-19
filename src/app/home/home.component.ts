af10feeb8bbf39f6861d27c1e7cafab2648b2af5
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-19 18:58:16 +0100

    update home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,6 @@
 import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
-import { LoginResponse } from '@app/_models';
-import { AccountService } from '@app/_services';
 import { HeaderComponent } from '@app/header/header.component';
 
 @Component({

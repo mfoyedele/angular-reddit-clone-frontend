5695f7930a51b7620715a2b499713ee30c019da9
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-22 22:33:25 +0100

    Update post.service component

--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -1,2 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostService {
+    constructor(private http: HttpClient) {}
+}

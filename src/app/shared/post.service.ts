65270009d99a841b6f82186348c07240b8e2716e
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-22 22:37:31 +0100

    post-model class added

--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class PostService {
     constructor(private http: HttpClient) {}
+
+    getAllPosts(): Observable<Array<PostModel>> {
+        return this.http.get<Array<PostModel>>('http://localhost:8080/api/posts/');
+      }
 }

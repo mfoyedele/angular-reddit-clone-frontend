f97377511ce24a8933b178f782a76c549395585c
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-22 23:11:02 +0100

    Update home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,12 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '@app/header/header.component';
 import { PostModel } from '@app/shared/post-model';
 import { PostService } from '@app/shared/post.service';
+import { PostTileComponent } from '@app/shared/post-tile/post-tile.component';
 
 @Component({
     templateUrl: 'home.component.html', styleUrls: ['home.component.css'],
     standalone: true,    
-    imports: [NgClass, NgIf, HeaderComponent]
+    imports: [NgClass, NgIf, HeaderComponent, PostTileComponent]
 })
 export class HomeComponent implements OnInit{
 

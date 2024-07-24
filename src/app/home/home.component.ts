56e2be7b4772b59d4289edcba30f677eb7e09445
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-24 17:18:01 +0100

    Update home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from '@app/header/header.component';
 import { PostModel } from '@app/shared/post-model';
 import { PostService } from '@app/shared/post.service';
 import { PostTileComponent } from '@app/shared/post-tile/post-tile.component';
+import { SideBarComponent } from '@app/shared/side-bar/side-bar.component';
 
 @Component({
     templateUrl: 'home.component.html', styleUrls: ['home.component.css'],
     standalone: true,    
-    imports: [NgClass, NgIf, HeaderComponent, PostTileComponent]
+    imports: [NgClass, NgIf, HeaderComponent, PostTileComponent, SideBarComponent]
 })
 export class HomeComponent implements OnInit{
 
--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,10 +1,13 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+  styleUrls: ['./side-bar.component.css'],
+  standalone: true,    
+  imports: [NgClass, NgIf]
 })
 export class SideBarComponent implements OnInit {
 

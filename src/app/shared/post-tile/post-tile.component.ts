5867930afa3e889b6720464bed46927f94bfde7a
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-24 17:10:30 +0100

    Update post-tile component

--- src/app/shared/post-tile/post-tile.component.ts
+++ src/app/shared/post-tile/post-tile.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { CommonModule, NgClass, NgIf } from '@angular/common';
 import { PostService } from '../post.service';
 import { PostModel } from '../post-model';
+import { VoteButtonComponent } from '../vote-button';
 import { faComments } from '@fortawesome/free-solid-svg-icons';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-post-tile',
   templateUrl: './post-tile.component.html',
   styleUrls: ['./post-tile.component.css'],
   standalone: true,
-  imports: [NgClass, NgIf, CommonModule],
+  imports: [NgClass, NgIf, CommonModule, RouterLink, FontAwesomeModule, VoteButtonComponent],
   encapsulation: ViewEncapsulation.None,
 })
 export class PostTileComponent implements OnInit {
@@ -23,7 +25,7 @@ export class PostTileComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  goToPost(id: number): void {
+  goToPost(id: number | undefined): void {
     this.router.navigateByUrl('/view-post/' + id);
   }
 }

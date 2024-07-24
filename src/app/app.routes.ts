710b6a16aaf1fc21726d42ca21ffa8ac5f8e5906
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-24 19:12:19 +0100

    create-subreddit component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,18 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from './home';
 import { LoginComponent, RegisterComponent } from './account';
 import { authGuard } from './_helpers';
+import { ViewPostComponent } from "./post/view-post";
+import { UserProfileComponent } from "./user-profile";
+import { CreatePostComponent } from "./post/create-post";
 
-// const usersRoutes = () => import('./users/users.routes').then(x => x.USERS_ROUTES);
 
 export const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
-    // { path: 'users', loadChildren: usersRoutes, canActivate: [authGuard] },
+    { path: 'view-post/:id', component: ViewPostComponent },
+    { path: 'user-profile/:name', component: UserProfileComponent, canActivate: [authGuard] },
+    { path: 'list-subreddits', component: ListSubredditsComponent },
+    { path: 'create-post', component: CreatePostComponent, canActivate: [authGuard] },
+    { path: 'create-subreddit', component: CreateSubredditComponent, canActivate: [authGuard] },
     { path: 'account/login', component: LoginComponent },
     { path: 'account/register', component: RegisterComponent },
 

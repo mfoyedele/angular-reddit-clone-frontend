bb314d980a8d8100615873a89401508d31808c5f
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-24 17:27:06 +0100

    update subreddit component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,11 +6,12 @@ import { PostModel } from '@app/shared/post-model';
 import { PostService } from '@app/shared/post.service';
 import { PostTileComponent } from '@app/shared/post-tile/post-tile.component';
 import { SideBarComponent } from '@app/shared/side-bar/side-bar.component';
+import { SubredditSideBarComponent } from '@app/shared/subreddit-side-bar';
 
 @Component({
     templateUrl: 'home.component.html', styleUrls: ['home.component.css'],
     standalone: true,    
-    imports: [NgClass, NgIf, HeaderComponent, PostTileComponent, SideBarComponent]
+    imports: [NgClass, NgIf, HeaderComponent, PostTileComponent, SideBarComponent, SubredditSideBarComponent]
 })
 export class HomeComponent implements OnInit{
 

c914bbaef354bed17878a8f114eb9d0dc966c081
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-24 20:18:48 +0100

    Update app routes component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,15 +6,17 @@ import { authGuard } from './_helpers';
 import { ViewPostComponent } from "./post/view-post";
 import { UserProfileComponent } from "./user-profile";
 import { CreatePostComponent } from "./post/create-post";
+import { ListSubredditsComponent } from "./subreddit/list-subreddits";
+import { CreateSubredditComponent } from "./subreddit/create-subreddit";
 
 
 export const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'view-post/:id', component: ViewPostComponent },
     { path: 'user-profile/:name', component: UserProfileComponent, canActivate: [authGuard] },
-    // { path: 'list-subreddits', component: ListSubredditsComponent },
+    { path: 'list-subreddits', component: ListSubredditsComponent },
     { path: 'create-post', component: CreatePostComponent, canActivate: [authGuard] },
-    // { path: 'create-subreddit', component: CreateSubredditComponent, canActivate: [authGuard] },
+    { path: 'create-subreddit', component: CreateSubredditComponent, canActivate: [authGuard] },
     { path: 'account/login', component: LoginComponent },
     { path: 'account/register', component: RegisterComponent },
 

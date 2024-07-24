886aed2b6d48fddd1d29395f2082a9a5d0bf00f4
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-24 20:14:21 +0100

    Update list-subreddit component

--- src/app/subreddit/list-subreddits/list-subreddits.component.ts
+++ src/app/subreddit/list-subreddits/list-subreddits.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule, NgClass, NgIf } from '@angular/common';
 import { SubredditModel } from '../subreddit-response';
 import { SubredditService } from '../subreddit.service';
 import { throwError } from 'rxjs';
@@ -6,7 +7,9 @@ import { throwError } from 'rxjs';
 @Component({
   selector: 'app-list-subreddits',
   templateUrl: './list-subreddits.component.html',
-  styleUrls: ['./list-subreddits.component.css']
+  styleUrls: ['./list-subreddits.component.css'],
+  standalone: true,
+  imports: [NgClass, NgIf, CommonModule]
 })
 export class ListSubredditsComponent implements OnInit {
 

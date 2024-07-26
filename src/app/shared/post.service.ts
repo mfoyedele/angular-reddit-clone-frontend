80cbc2c39b8ea506f14e07c9aeed345e47878042
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-26 12:28:10 +0100

    Update post service component

--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { PostModel } from './post-model';
 import { Observable } from 'rxjs';
 import { CreatePostPayload } from '../post/create-post/create-post.payload';
+import { environment } from '@environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,18 +13,18 @@ export class PostService {
   constructor(private http: HttpClient) { }
 
   getAllPosts(): Observable<Array<PostModel>> {
-    return this.http.get<Array<PostModel>>(`${environment.apiUrl}/api/posts');
+    return this.http.get<Array<PostModel>>(`${environment.apiUrl}/api/posts`);
   }
 
   createPost(postPayload: CreatePostPayload): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/api/posts', postPayload);
+    return this.http.post(`${environment.apiUrl}/api/posts`, postPayload);
   }
 
   getPost(id: number | undefined): Observable<PostModel> {
-    return this.http.get<PostModel>(`${environment.apiUrl}/api/posts/' + id);
+    return this.http.get<PostModel>(`${environment.apiUrl}/api/posts/` + id);
   }
 
   getAllPostsByUser(name: string): Observable<PostModel[]> {
-    return this.http.get<PostModel[]>(`${environment.apiUrl}/api/posts/by-user' + name);
+    return this.http.get<PostModel[]>(`${environment.apiUrl}/api/posts/by-user` + name);
   }
 }

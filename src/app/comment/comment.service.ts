f64cd3e17b58e4a534a49e22b9fb82301637bf9f
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-26 12:27:11 +0100

    Update comment service component

--- src/app/comment/comment.service.ts
+++ src/app/comment/comment.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommentPayload } from './comment.payload';
 import { Observable } from 'rxjs';
+import { environment } from '@environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,14 @@ export class CommentService {
   constructor(private httpClient: HttpClient) { }
 
   getAllCommentsForPost(postId: number): Observable<CommentPayload[]> {
-    return this.httpClient.get<CommentPayload[]>(`${environment.apiUrl}/api/comments/by-post' + postId);
+    return this.httpClient.get<CommentPayload[]>(`${environment.apiUrl}/api/comments/by-post` + postId);
   }
 
   postComment(commentPayload: CommentPayload): Observable<any> {
-    return this.httpClient.post<any>(`${environment.apiUrl}/api/comments', commentPayload);
+    return this.httpClient.post<any>(`${environment.apiUrl}/api/comments`, commentPayload);
   }
 
   getAllCommentsByUser(name: string) {
-    return this.httpClient.get<CommentPayload[]>(`${environment.apiUrl}/api/comments/by-user' + name);
+    return this.httpClient.get<CommentPayload[]>(`${environment.apiUrl}/api/comments/by-user` + name);
   }
 }

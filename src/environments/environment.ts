7a26c105e89f0bb008d148b53c3b9ba9a8c543f7
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-26 12:30:03 +0100

    Update environment component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    apiUrl: `${environment.apiUrl}'
+    apiUrl: 'http://localhost:8080'
   };
   
   /*

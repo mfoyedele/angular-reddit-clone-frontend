768db89c0953f63dabc9f7d3d1f5218e1ffbc8c5
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-29 14:20:17 +0100

    Update vote service

--- src/app/shared/vote.service.ts
+++ src/app/shared/vote.service.ts
@@ -1,31 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient, HttpHeaders } from '@angular/common/http';
-// import { VotePayload } from './vote-button/vote-payload';
-// import { Observable } from 'rxjs';
-// import { environment } from '@environments/environment';
-
-// const httpHeaders: HttpHeaders = new HttpHeaders({
-//   Authorization: this.getAuthenticationToken
-// });
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class VoteService {
-
-//   constructor(private http: HttpClient) { }
-
-//   vote(votePayload: VotePayload): Observable<any> {
-//     return this.http.post(`${environment.apiUrl}/api/votes`, votePayload, { headers: httpHeaders });
-//   }
-
-//   getAuthenticationToken() {
-//     return localStorage.getItem('authenticationToken');
-//   }
-// }
-
-
-
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { VotePayload } from './vote-button/vote-payload';

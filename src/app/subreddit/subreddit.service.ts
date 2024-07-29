9e0dfc7da7741bebc403c983bfeae34ea3e9bc5e
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-07-29 14:28:08 +0100

    Update subreddit service

--- src/app/subreddit/subreddit.service.ts
+++ src/app/subreddit/subreddit.service.ts
@@ -4,9 +4,6 @@ import { SubredditModel } from './subreddit-response';
 import { Observable } from 'rxjs';
 import { environment } from '@environments/environment';
 
-const httpHeaders: HttpHeaders = new HttpHeaders({
-  Authorization: 'Bearer JWT-token'
-});
 @Injectable({
   providedIn: 'root'
 })
@@ -14,11 +11,24 @@ export class SubredditService {
   constructor(private http: HttpClient) { }
 
   getAllSubreddits(): Observable<Array<SubredditModel>> {
-    return this.http.get<Array<SubredditModel>>(`${environment.apiUrl}/api/subreddit`);
+    const token = this.getAuthenticationToken();
+    const httpHeaders: HttpHeaders = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.get<Array<SubredditModel>>(`${environment.apiUrl}/api/subreddit`, { headers: httpHeaders });
   }
 
   createSubreddit(subredditModel: SubredditModel): Observable<SubredditModel> {
-    return this.http.post<SubredditModel>(`${environment.apiUrl}/api/subreddit`,
-      subredditModel, { headers: httpHeaders } );
+    const token = this.getAuthenticationToken();
+    const httpHeaders: HttpHeaders = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.post<SubredditModel>(`${environment.apiUrl}/api/subreddit`, subredditModel, { headers: httpHeaders });
+  }
+
+  private getAuthenticationToken(): string | null {
+    return localStorage.getItem('authenticationToken');
   }
 }
